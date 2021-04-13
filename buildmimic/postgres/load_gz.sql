31099fd313876d17f426251ea06a00af195dda7e
ryukyunghun <rkyunghun@khryuui-MacBookPro.local>
2021-04-13 14:50:48 +0900

    fix : load_gz.sql

--- buildmimic/postgres/load_gz.sql
+++ buildmimic/postgres/load_gz.sql
@@ -5,15 +5,13 @@
 -- To run from a terminal:
 --  psql "dbname=<DBNAME> user=<USER>" -v mimic_data_dir=<PATH TO DATA DIR> -f load_gz.sql
 
--- core schema
-\cd ::mimic_data_core
-
 -- making sure that all tables are emtpy and correct encoding is defined -utf8- 
 SET CLIENT_ENCODING TO 'utf8';
 
 DELETE FROM mimic_core.admissions; 
 DELETE FROM mimic_core.patients; 
 DELETE FROM mimic_core.transfers;
+
 DELETE FROM mimic_hosp.d_hcpcs;
 DELETE FROM mimic_hosp.diagnoses_icd;
 DELETE FROM mimic_hosp.d_icd_diagnoses;
@@ -31,6 +29,7 @@ DELETE FROM mimic_hosp.poe;
 DELETE FROM mimic_hosp.prescriptions;
 DELETE FROM mimic_hosp.procedures_icd;
 DELETE FROM mimic_hosp.services;
+
 DELETE FROM mimic_icu.chartevents;
 DELETE FROM mimic_icu.datetimeevents;
 DELETE FROM mimic_icu.d_items;
@@ -39,12 +38,16 @@ DELETE FROM mimic_icu.inputevents;
 DELETE FROM mimic_icu.outputevents;
 DELETE FROM mimic_icu.procedureevents;
 
+-- core schema
+
+\cd ::/home/linewalks/mimic-iv-0.4/core
+
 \COPY mimic_core.admissions         FROM PROGRAM 'gzip -dc admissions.csv.gz' DELIMITER ',' CSV HEADER NULL '';
 \COPY mimic_core.patients           FROM PROGRAM 'gzip -dc patients.csv.gz'   DELIMITER ',' CSV HEADER NULL '';
 \COPY mimic_core.transfers          FROM PROGRAM 'gzip -dc transfers.csv.gz'  DELIMITER ',' CSV HEADER NULL '';
 
 -- hosp schema
-\cd ::mimic_data_hosp
+\cd ::/home/linewalks/mimic-iv-0.4/hosp
 
 \COPY mimic_hosp.d_hcpcs            FROM PROGRAM 'gzip -dc d_hcpcs.csv.gz' DELIMITER ',' CSV HEADER NULL '';
 \COPY mimic_hosp.diagnoses_icd      FROM PROGRAM 'gzip -dc diagnoses_icd.csv.gz' DELIMITER ',' CSV HEADER NULL '';
@@ -65,7 +68,7 @@ DELETE FROM mimic_icu.procedureevents;
 \COPY mimic_hosp.services           FROM PROGRAM 'gzip -dc services.csv.gz' DELIMITER ',' CSV HEADER NULL '';
 
 -- icu schema
-\cd ::mimic_data_icu
+\cd ::/home/linewalks/mimic-iv-0.4/icu
 
 \COPY mimic_icu.chartevents         FROM PROGRAM 'gzip -dc chartevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
 \COPY mimic_icu.datetimeevents      FROM PROGRAM 'gzip -dc datetimeevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
